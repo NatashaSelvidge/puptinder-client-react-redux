1304d54db1ca95ba339f9cb12ed4c4c72aa2c27a
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-04-11 16:54:43 -0700

    mapped state to props and displayed dogs

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Dog extends Component {
   render() {
+    const { name, image_url } = this.props;
     return (
       <div>
-        Dog
+        <img src={image_url} alt={name} />
+        <p>{name}</p>
       </div>
-    )
+    );
   }
 }
 
-export default Dog
+export default Dog;
