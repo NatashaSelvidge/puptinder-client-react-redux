bcda1d5726f85f5b1ae2fc6938a93bd85b5584ec
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-04-11 14:17:04 -0700

    set initial state in my dogs reducer

---
+++ src/reducers/dogsReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  dogs: [],
+  loading: true,
+};
+
+const dogsReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default dogsReducer;
