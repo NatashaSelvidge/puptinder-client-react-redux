f7c8a28251838d2c209622f1cb6fe4a39a8a36e4
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-04-11 14:51:20 -0700

    added about and dogs to route path

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from "react";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 import Nav from "./components/Nav";
+// import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
+import CenterContainer from "./components/CenterContainer";
+// import Form from "./components/Form";
+import ErrorPage from "./components/ErrorPage";
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <Nav />
-        <div className="App">
-          <h1>Hello World</h1>
-        </div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/dogs" component={CenterContainer} />
+          <Route component={ErrorPage} />
+        </Switch>
+        <br />
+        <Footer />
       </Router>
     );
   }
--- src/reducers/dogsReducer.js
+++ src/reducers/dogsReducer.js
@@ -3,7 +3,7 @@ const initialState = {
   loading: true,
 };
 
-const dogsReducer = (state, action) => {
+const dogsReducer = (state = initialState, action) => {
   switch (action.type) {
     default:
       return state;
