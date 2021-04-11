6e2cb4ab02dd966205029e1775834a531dd8a892
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-04-11 14:20:28 -0700

    Created nav bar and added router

---
+++ src/components/CenterContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class CenterContainer extends Component {
+  render() {
+    return <div>List</div>;
+  }
+}
+
+export default CenterContainer;
---
+++ src/components/Dog.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Dog extends Component {
+  render() {
+    return (
+      <div>
+        Dog
+      </div>
+    )
+  }
+}
+
+export default Dog
