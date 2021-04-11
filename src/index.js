3ca3cf126f3a10e4cfeb06e5566a6242787ea189
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-04-11 14:18:00 -0700

    created store and added provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import dogsReducer from "./reducers/dogsReducer";
+
+const store = createStore(dogsReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
