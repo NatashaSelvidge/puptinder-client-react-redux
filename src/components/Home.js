1a09bee6fd055b3b7a4e6ab4208e56b2668983ac
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-04-20 12:49:36 -0700

    Added Jumbotron Bootstrap and animation to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
+import "../App.css";
+import { Animated } from "react-animated-css";
 export class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcom To PupTinder</h1>
+      <div className="Home">
+        <Jumbotron className="home__Jumbotron">
+          <div className="home__content">
+            <h1>Find your new furry family member!</h1>
+            <Animated
+              animationIn="bounceInLeft"
+              isVisible={true}
+            >
+              <h3>Swipe right on the pups you like. </h3>
+            </Animated>
+            <p>
+              <Link to="/dogs">
+                <Button className="home__button">DOGS</Button>
+              </Link>
+            </p>
+          </div>
+        </Jumbotron>
       </div>
     );
   }
 }
 
-export default Home
+export default Home;
